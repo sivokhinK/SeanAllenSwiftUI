16688bbf6f8cec17481782712656db6687422cf4
Kirill Sivokhin <kirill.sivokhin@vk.com>
2023-09-09 21:55:39 +0300

    Barcode Scanner dummy design

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,37 @@
+//
+//  BarcodeScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Kirill Sivokhin on 09.09.2023.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer().frame(height: 60)
+                
+                Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text("Not Yet Scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+struct BarcodeScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+    }
+}
